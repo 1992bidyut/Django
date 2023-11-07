c5ff22bc4430c987b4a092a9728f811ecef0a101
Bidyut <bidyut@divine-id.net>
2023-11-07 11:55:03 +0600

    django channel for realtime chat application

--- AsyncDjangoApp/App/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class JobForm(forms.Form):
-    job_name = forms.CharField(label='Job name', required=True)
---
+++ chatproject/chatproject/asgi.py
@@ -0,0 +1,28 @@
+"""
+ASGI config for chatproject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+# 👇 1. Update the below import lib
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from chatapp.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproject.settings')
+
+# 👇 2. Update the application var
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        ),
+})
