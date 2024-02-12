84e22323bbb387b5f11c8316e2649847eca56980
Bidyut <bidyut@divine-id.net>
2024-02-12 11:17:33 +0600

    init

--- chatproject/chatapp/urls.py
+++ chatproject/chatapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('chat_data/', views.chat_data, name='chat_data'),
+    path('mqtt/', views.mqtt, name='mqtt'),
 ]
